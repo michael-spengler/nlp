fe18926496a50c90c0acdeb0e259605198067c32
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2020-08-04 15:47:27 +0200

    🦕   using http://deno.land/x/request now

--- nlp.ts
+++ nlp.ts
@@ -1,16 +1,16 @@
-import api from 'https://deno.land/x/api/index.ts' 
+import { Request } from 'https://deno.land/x/request@1.1.0/request.ts' 
 import { IDocument, IAnswer } from "https://raw.githubusercontent.com/michael-spengler/nlp/master/interfaces.ts"
 
 export class NLP {
     
     public static async train(documents: IDocument[], answers: IAnswer[], languageCodes: string[], nlpProvider: string = "https://fancy-chats.com:4443"): Promise<any> {
-        const result = await api.post(`${nlpProvider}/train/`, { documents, answers, languageCodes })
+        const result = await Request.post(`${nlpProvider}/train/`, { documents, answers, languageCodes })
         return result
     }
     
     public static async getResponse(input: string, languageCode: string, clientId: string, nlpProvider: string = "https://fancy-chats.com:4443"): Promise<any> {
         const url = `${nlpProvider}/process/input/${input}/languageCode/${languageCode}/clientId/${clientId}`
-        const result = await api.get(url)
+        const result = await Request.get(url)
         return result
     }
 
